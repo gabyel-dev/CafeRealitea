4d56ea99ad0b8a88c887d03984c775cefe82ee56
gabyel-dev <184721807+gabyel-dev@users.noreply.github.com>
2025-08-19 13:28:43 +0800

    test

--- Server/main.py
+++ Server/main.py
@@ -4,13 +4,15 @@ from Controllers.auth_controller import auth_bp  # adjust path if needed
 from Models.database import get_db_conn
 from config import Config
 from utils.hash_passwords import bcrypt
+from flask_session import Session
 
 get_db_conn()
 
 app = Flask(__name__)
 app.secret_key = Config.SECRET_KEY
-CORS(app, supports_credentials=True, origins="https://cafe-realitea.vercel.app/") 
+CORS(app, supports_credentials=True) 
 bcrypt.init_app(app)
+Session(app)
 
 
 app.register_blueprint(auth_bp)
