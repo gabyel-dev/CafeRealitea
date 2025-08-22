7e16a92f91c11f887855dc2794590dc9a7c26e8e
gabyel-dev <184721807+gabyel-dev@users.noreply.github.com>
2025-08-22 15:50:02 +0800

    added usrs management

--- Admin/src/routes/Dashboard/Admin/pages/UsersManagement.jsx
+++ Admin/src/routes/Dashboard/Admin/pages/UsersManagement.jsx
@@ -1,12 +1,25 @@
 import AdminSidePanel from "../../../../components/AdminSidePanel";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function UsersManagement({ activeTab, setActiveTab }) {
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        axios.get('https://caferealitea.onrender.com/users_account')
+        .then((res) => {
+            setUsers(res.data)
+        })
+    })
     return (
         <>
             <AdminSidePanel activeTab={activeTab} setActiveTab={setActiveTab} />
             <div className="h-screen ml-80">
-
+                {users.map((user) => (
+                    <div key={user.id}>
+                        <p>{user.first_name} {user.last_name} {user.email} {user.role}</p>
+                    </div>
+                ))}
             </div>
         </>
     )
