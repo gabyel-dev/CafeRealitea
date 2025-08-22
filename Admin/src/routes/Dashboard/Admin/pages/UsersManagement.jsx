dc25c9ff3eea3a628a0d13d959d29f9362d5a498
gabyel-dev <184721807+gabyel-dev@users.noreply.github.com>
2025-08-22 15:51:56 +0800

    asd

--- Admin/src/routes/Dashboard/Admin/pages/UsersManagement.jsx
+++ Admin/src/routes/Dashboard/Admin/pages/UsersManagement.jsx
@@ -10,7 +10,7 @@ export default function UsersManagement({ activeTab, setActiveTab }) {
         .then((res) => {
             setUsers(res.data)
         })
-    })
+    }, [])
     return (
         <>
             <AdminSidePanel activeTab={activeTab} setActiveTab={setActiveTab} />
